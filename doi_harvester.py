47de4292a8636ad973ceeb69d09eac69bb587490
wtee <wtee@users.noreply.github.com>
2019-06-14 14:22:33 -0500

    Modify to user cursor instead of offset
    Crossref's API doesn't accept offsets higher than 10,000,
    therefor I rewrote the script to use the cursor option recommended
    for deep paging.

--- doi_harvester.py
+++ doi_harvester.py
@@ -4,22 +4,36 @@ from time import sleep
 
 import requests
 
-offset = 0
-total_records = 38204
+# total_records: 38204
 
-out_file = "crossref_metadata_json.txt"
+cursor = "*"
 
-while offset < total_records:
-    print(offset)
-    url = f"https://api.crossref.org/prefixes/10.31274/works?offset={offset}"
+out_file = "crossref_metadata_json.json"
+
+with open(out_file, "w", encoding="utf-8") as fh:
+    fh.write("[")
+
+while True:
+    print(cursor)
+    url = f"https://api.crossref.org/prefixes/10.31274/works?cursor={cursor}"
     r = requests.get(url)
 
     if r.ok:
         with open(out_file, "a", encoding="utf-8") as fh:
-            fh.write(r.text)
-        offset +=20
+            fh.write(f"{r.text},")
+
+        cursor = r.json()["message"]["next-cursor"]
+
+        if len(r.json()["message"]["items"]) < 20:
+            print("Done!")
+            break
+
     else:
         print(r.status_code)
-    # We've got a lot records, so let's play nice and not blast the API
-    # with 1,910 requests over just a minute or two.
-    sleep(5)
+        print(r.text)
+        break
+    
+    sleep(2)
+
+with open(out_file, "a", encoding="utf-8") as fh:
+    fh.write("]")
