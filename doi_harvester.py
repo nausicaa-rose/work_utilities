9243d63bb779d659887b66a4ba78fc915b83200b
wtee <wtee@users.noreply.github.com>
2019-06-14 14:50:55 -0500

    Rewrite to use crossref API library
    When trying to use the cursor-based version, at a certain point
    the Crossref API would choke when given the cursor it returned
    for next cursor, so just going try to use the Python library
    crossref.

--- doi_harvester.py
+++ doi_harvester.py
@@ -1,39 +1,11 @@
-"""Download all Iowa State-generated DOIs from Crossref."""
+from crossref.restful import Prefixes
 
-from time import sleep
+prefixes = Prefixes()
 
-import requests
+wall = prefixes.works("10.31274").all()
 
-# total_records: 38204
-
-cursor = "*"
-
-out_file = "crossref_metadata_json.json"
-
-with open(out_file, "w", encoding="utf-8") as fh:
+with open("out.json", "w", encoding="utf-8") as fh:
     fh.write("[")
-
-while True:
-    print(cursor)
-    url = f"https://api.crossref.org/prefixes/10.31274/works?cursor={cursor}"
-    r = requests.get(url)
-
-    if r.ok:
-        with open(out_file, "a", encoding="utf-8") as fh:
-            fh.write(f"{r.text},")
-
-        cursor = r.json()["message"]["next-cursor"]
-
-        if len(r.json()["message"]["items"]) < 20:
-            print("Done!")
-            break
-
-    else:
-        print(r.status_code)
-        print(r.text)
-        break
-    
-    sleep(2)
-
-with open(out_file, "a", encoding="utf-8") as fh:
+    for w in wall:
+        fh.write(f"{w},")
     fh.write("]")
