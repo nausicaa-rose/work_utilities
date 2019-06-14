53e0d4fd43475baaf8924f2498e4aa44476895a3
wtee <wtee@users.noreply.github.com>
2019-06-14 17:24:07 -0500

    Make a few tweaks

--- doi_harvester.py
+++ doi_harvester.py
@@ -4,8 +4,8 @@ prefixes = Prefixes()
 
 wall = prefixes.works("10.31274").all()
 
-with open("out.json", "w", encoding="utf-8") as fh:
-    fh.write("[")
+with open("out.txt", "w", encoding="utf-8") as fh:
+    fh.write("doi_md = [\n")
     for w in wall:
-        fh.write(f"{w},")
-    fh.write("]")
+        fh.write(f"    {w},\n")
+    fh.write("\n]")
